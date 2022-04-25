dba5b33590947b0108668d43d3755ca644a40479
Nikolaj JK <kagenogmig@gmail.com>
2022-04-25 22:25:22 +0200

    svelte + fontawesome

---
+++ thread-art-website/src/routes/index.svelte
@@ -0,0 +1,65 @@
+<script>
+    let files;
+    let errorMessage = null;
+
+    function upload() {
+        if(!files || !files[0]) {
+            return
+        }
+        
+        let url = "/upload";
+        let formData = new FormData();
+
+        formData.append("image", files[0]);
+        console.log("hello")
+
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        })
+            .then(() => {
+                /* Done. Inform the user */
+            })
+            .catch(() => {
+                /* Error. Inform the user */
+            });
+    }
+</script>
+
+<svelte:head>
+    <title>thread-art-website</title>
+</svelte:head>
+
+<div class="section">
+    <div class="container is-max-desktop">
+        <h1 class="title">thread-art-website</h1>
+        <p class="subtitle">Hello</p>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container is-max-desktop">
+        <div class="file is-boxed is-primary is-large">
+            <label class="file-label">
+                <input
+                    class="file-input"
+                    id="fileupload"
+                    type="file"
+                    name="resume"
+                    accept="image/png, image/jpeg, image/webp"
+                    bind:files
+                    on:change="{upload}"
+                />
+                <span class="file-cta">
+                    <span class="file-icon">
+                        <i class="fas fa-upload" />
+                    </span>
+                    <span class="file-label"> Choose an image... </span>
+                </span>
+                {#if errorMessage}
+                    <span class="file-label">{errorMessage}</span>
+                {/if}
+            </label>
+        </div>
+    </div>
+</div>
--- threadArtServer/Todo.md
+++
@@ -1,5 +0,0 @@
-# Todo - threadArtServer
-- [ ] Modtag billede med HTTP POST
-- [ ] Downscale og vælg center del af billedet, så det har en størrelse på 400x400. (I virkeligheden skal den måske downscales i browseren, så brugeren bestemmer hvad der skal være på billedet. Her kan man selvfølgelig bede dem om selv at gøre det)
-- [ ] Valider at der findes en version af billedet på 400x400.
-- [ ] osv..
