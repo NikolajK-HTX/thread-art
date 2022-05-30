84177e7df7581aa5ef81b2db6fe26dcd28cb8a66
NikolajK-HTX <kagenogmig@gmail.com>
2022-05-30 21:18:01 +0200

    Update main.go

--- thread-art-go/main.go
+++ thread-art-go/main.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	"math"
 	"os"
 )
 
+var imagePath = "../selfie.jpg"
+var numberOfPoints = 200
+
+func constrain(x, a, b int) int {
+	switch {
+	case x < a:
+		return a
+	case x > b:
+		return b
+	default:
+		return x
+	}
+}
+
 func main() {
-	fmt.Println("Hello!")
-	f, _ := os.Open("../selfie.jpg")
+	f, _ := os.Open(imagePath)
 
 	// using image.DecodeConfig(f) consumes all of f
 	// so you need to seek to (0, 0) if open the file
@@ -24,7 +38,7 @@ func main() {
 	height := decodedImageConfig.Height
 
 	if width != 400 || height != 400 {
-		fmt.Println("Image dimensions not supported.")
+		fmt.Printf("Image at '%v' dimensions not supported.\n", imagePath)
 		fmt.Println("Only square by 400 pixels is accepted.")
 
 		os.Exit(1)
@@ -51,6 +65,28 @@ func main() {
 	}
 	fmt.Println(sum)
 
-	// test bresenham
-	fmt.Printf("%v", Bresenham(1, 1, 10, 10))
+	// create points outlining circle
+	var circle []Point
+	for i := 0; i < numberOfPoints; i++ {
+		var x, y int
+		angle := float64(math.Pi)*2.0/float64(numberOfPoints) * float64(i)
+		x = constrain(int(math.Cos(angle)*float64(width)/2.0 + float64(width)/2.0), 0, width)
+		y = constrain(int(math.Sin(angle)*float64(height)/2.0 + float64(height)/2.0), 0, width)
+		circle = append(circle, Point{x, y})
+	}
+
+	// create a dictionary of all possible lines in circle.
+	// it is more performant to create this once and have the
+	// results at hand than creating them when needed to check
+	// the weight of possible lines and decide which to draw a line on.
+
+	var lines map[string]Point
+	lines = make(map[string]Point)
+
+	var x0, y0, x1, y1 int
+	x0 = circle[2].X
+	y0 = circle[2].Y
+	x1 = circle[102].X
+	y1 = circle[102].Y
+	fmt.Printf("%v", Bresenham(x0, y0, x1, y1))
 }
