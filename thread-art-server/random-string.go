5d630ddc5da30b6ecf5e7b3a38d1565761ead420
Nikolaj J-K <kagenogmig@gmail.com>
2022-06-13 13:35:29 +0200

    Save image with random id

---
+++ thread-art-server/random-string.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+func RandomString(length int) string {
+	clen := len(letters)
+	b := make([]byte, length)
+	r := make([]byte, length) // storage for random bytes.
+
+	// if seed is not set it is 1, which makes for not
+	// so random numbers
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(r)
+
+	for i, rb := range r {
+		c := int(rb)
+		b[i] = letters[c%clen]
+	}
+
+	return string(b)
+}
